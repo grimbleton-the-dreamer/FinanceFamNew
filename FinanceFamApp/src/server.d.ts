79abfdaa6dbfe9deccebc7f05f7ea64c74a1705e
grimbleton-the-dreamer <mattk6564@aol.com>
2024-12-05 15:01:17 -0500

    Found out how to clean

--- FinanceFamApp/src/server.d.ts
+++
@@ -1,3 +0,0 @@
-import pkg from 'pg';
-declare const pool: pkg.Pool;
-export default pool;
--- FinanceFamApp/src/stores/useAuthStore.d.ts
+++
@@ -1,8 +0,0 @@
-import type { User } from './types';
-export declare const useAuthStore: import("pinia").StoreDefinition<"auth", {
-    currentUser: User | null;
-    error: string | null;
-}, {}, {
-    login(username: string, password: string): void;
-    logout(): void;
-}>;
